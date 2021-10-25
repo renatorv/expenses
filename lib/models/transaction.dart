bec208cc3076eaa0e08dfde8ff084a359246f52b
Renato Alves <renato@volus.com>
2021-10-25 17:33:18 -0300

    final da aula 33

---
+++ lib/models/transaction.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  
+  final String id;
+  final String title;
+  final double value;
+  final DateTime date;
+
+  Transaction({
+    @required this.id,
+    @required this.title,
+    @required this.value,
+    @required this.date,
+  });
+}
+
+
+// class Transaction {
+//   final String id;
+//   final String title;
+//   final double value;
+//   final DateTime date;
+
+//   Transaction({
+//     required this.id,
+//     required this.title,
+//     required this.value,
+//     required this.date,
+//   });
+// }
+
+// sdk: ">=2.12.0 <3.0.0"
