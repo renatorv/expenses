24e4cc724974791b6e4a2d79c392cfdada2b669a
Renato Alves <renato@volus.com>
2021-10-27 09:11:04 -0300

    inicio da aula 123

---
+++ lib/components/chart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
